6ca4edd8f5a50f3565c66b0256f14ed7259cff98
aziz-marzougui106 <mohamed.marzougui@epfl.ch>
2026-04-18 16:10:38 +0200

    interactive TextField & CheckBox

--- lib/screens/pages/profile_page.dart
+++ lib/screens/pages/profile_page.dart
@@ -1,9 +1,33 @@
 import 'package:flutter/material.dart';
-class ProfilePage extends StatelessWidget {
+import 'package:flutter/services.dart';
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  TextEditingController controller=TextEditingController();
+  bool? isChecked=false;
+  @override
   Widget build(BuildContext context) {
-    return const SizedBox(child:Center(child:Text('ProfilePage')));
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child:Column(
+        children: [
+          TextField(
+            controller:controller,
+            decoration: InputDecoration(border:OutlineInputBorder()),
+            onEditingComplete: () => setState(() {}),
+            //onChanged: (value) => setState(() {}), //when the controller changes the state changes
+          ),
+          Text(controller.text),
+          Checkbox(tristate: true, value: isChecked, onChanged: (value) {controller.clear(); setState(() => isChecked=value);}),
+          CheckboxListTile(tristate: true, value: isChecked, onChanged:(value)  {controller.clear();setState(()=> isChecked=value);}),
+          
+        ],
+      )
+    );
   }
 }
